5051f7325eaaef48879d3842241e460811878c64
Connor Rigby <connorr@hey.com>
2022-05-10 06:17:10 -0600

    Add pullups to sdcard

--- firmware/esp_fw/main/main.c
+++ firmware/esp_fw/main/main.c
@@ -19,6 +19,10 @@
 #include <soc/gpio_struct.h>
 
 static const char *TAG = "CONEPROJ";
+#define PIN_NUM_MISO
+#define PIN_NUM_MOSI
+#define PIN_NUM_CLK
+#define PIN_NUM_SD_CS
 
 void spi_init()
 {   
@@ -58,14 +62,53 @@ void spi_deinit()
 void sdcard_init()
 {
     esp_err_t ret;
-    ret = sdspi_host_init();
-    sdspi_device_config_t config;
-    sdspi_dev_handle_t handle;
-    sdspi_host_init_device(config, handle);
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    // const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+    ESP_LOGI(TAG, "Using SPI peripheral");
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_NUM_SD_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+        // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
 }
 
 void sdcard_deinit()
 {
+    // All done, unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+
     sdspi_host_remove_device();
     sdspi_host_deinit();
 }
