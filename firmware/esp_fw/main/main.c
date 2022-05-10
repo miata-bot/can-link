e8bf11a5a6ed7b20170ceb0e8967b64ac0ad4a85
Connor Rigby <connorr@hey.com>
2022-05-10 15:28:40 -0600

    Ble scafolding

--- firmware/esp_fw/main/main.c
+++ firmware/esp_fw/main/main.c
@@ -14,6 +14,7 @@
 #include <esp_log.h>
 #include <esp_chip_info.h>
 #include <esp_spi_flash.h>
+#include <esp_system.h>
 
 #include <driver/spi_master.h>
 #include <driver/gpio.h>
@@ -22,128 +23,121 @@
 #include <esp_vfs_fat.h>
 #include <sdmmc_cmd.h>
 
-static void report(lua_State *L, int status);
+#include "ble.h"
+
 static void halt();
-static void mount_spiffs();
 
 static const char *TAG = "CONEPROJ";
+
 #define PIN_NUM_MISO 2
 #define PIN_NUM_MOSI 13
 #define PIN_NUM_CLK 14
-#define PIN_NUM_SD_CS 15
-const char mount_point[] = "/sdcard";
 
-void spi_init()
-{   
-    spi_bus_config_t buscfg={
-        .miso_io_num=PIN_NUM_MISO,
-        .mosi_io_num=PIN_NUM_MOSI,
-        .sclk_io_num=PIN_NUM_CLK,
-        .quadwp_io_num=-1,
-        .quadhd_io_num=-1,
+// SD Config
+#define PIN_NUM_SD_CS 15
+const char SD_MOUNT_POINT[] = "/sdcard";
+sdmmc_card_t SDCARD;
+
+static void spi_init()
+{
+    spi_bus_config_t buscfg = {
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
         .max_transfer_sz = 4000,
     };
 
-    //Initialize the SPI bus in HSPI mode. DMA channel might need changing later?
+    // Initialize the SPI bus in HSPI mode. DMA channel might need changing later?
     esp_err_t ret = spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
-    
+
     switch (ret)
     {
-        case ESP_OK:
-            break;
-        case ESP_FAIL:
-            ESP_LOGE(TAG, "Failed to initialize HSPI Host");
-            halt();
-            break;
-        case ESP_ERR_NOT_FOUND:
-            ESP_LOGE(TAG, "Failed to initialize HSPI Host: not found");
-            halt();
-            break;
-        default:
-            ESP_LOGE(TAG, "Failed to initialize HSPI Host (%s)", esp_err_to_name(ret));
-            halt();
+    case ESP_OK:
+        break;
+    case ESP_FAIL:
+        ESP_LOGE(TAG, "Failed to initialize HSPI Host");
+        halt();
+        break;
+    case ESP_ERR_NOT_FOUND:
+        ESP_LOGE(TAG, "Failed to initialize HSPI Host: not found");
+        halt();
+        break;
+    default:
+        ESP_LOGE(TAG, "Failed to initialize HSPI Host (%s)", esp_err_to_name(ret));
+        halt();
     }
 }
 
-void spi_deinit()
+static void spi_deinit()
 {
-    // spi_bus_free();
+    // causes panic for some reason
+    // spi_bus_free(HSPI_HOST);
 }
 
-void sdcard_init()
+static void sdcard_init()
 {
-    esp_err_t ret;
-    // If format_if_mount_failed is set to true, SD card will be partitioned and
-    // formatted in case when mounting fails.
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
         .format_if_mount_failed = false,
         .max_files = 5,
-        .allocation_unit_size = 16 * 1024
-    };
-    sdmmc_card_t *card;
-    ESP_LOGI(TAG, "Initializing SD card");
-
-    // Use settings defined above to initialize SD card and mount FAT filesystem.
-    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
-    // Please check its source code and implement error recovery when developing
-    // production applications.
-    ESP_LOGI(TAG, "Using SPI peripheral");
+        .allocation_unit_size = 16 * 1024};
 
     sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    ESP_LOGI(TAG, "Mounting SD filesystem");
 
     // This initializes the slot without card detect (CD) and write protect (WP) signals.
     // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
     sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
     slot_config.gpio_cs = PIN_NUM_SD_CS;
     slot_config.host_id = host.slot;
+    sdmmc_card_t *card = &SDCARD;
 
-    ESP_LOGI(TAG, "Mounting filesystem");
-    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+    // Mount the filesystem
+    esp_err_t ret = esp_vfs_fat_sdspi_mount(SD_MOUNT_POINT, &host, &slot_config, &mount_config, &card);
 
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount filesystem.");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
-                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
-        }
+    switch (ret)
+    {
+    case ESP_OK:
+        ESP_LOGI(TAG, "Filesystem mounted");
+        break;
+    case ESP_FAIL:
+        ESP_LOGE(TAG, "Failed to mount filesystem.");
+        return;
+    default:
+        ESP_LOGE(TAG, "Failed to initialize the card (%s). ", esp_err_to_name(ret));
         return;
     }
-    ESP_LOGI(TAG, "Filesystem mounted");
-        // Card has been initialized, print its properties
+
+    // Card has been initialized, print its properties
     sdmmc_card_print_info(stdout, card);
 }
 
-void sdcard_deinit()
+static void sdcard_deinit()
 {
-    // All done, unmount partition and disable SPI peripheral
-    // esp_vfs_fat_sdcard_unmount(mount_point, card);
-
-    // sdspi_host_remove_device();
-    // sdspi_host_deinit();
+    // unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(SD_MOUNT_POINT, &SDCARD);
 }
 
-void radio_init()
+static void radio_init()
 {
     // spi_bus_add_device
 }
 
-void radio_deinit()
+static void radio_deinit()
 {
     // spi_bus_remove_device()
 }
 
-void motor_init()
+static void motor_init()
 {
-    
 }
 
-void motor_deinit()
+static void motor_deinit()
 {
-
 }
 
-void twai_init()
+static void twai_init()
 {
     // //Initialize configuration structures using macro initializers
     // twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
@@ -167,7 +161,7 @@ void twai_init()
     // }
 }
 
-void twai_deinit()
+static void twai_deinit()
 {
     // //Stop the TWAI driver
     // if (twai_stop() == ESP_OK) {
@@ -186,19 +180,45 @@ void twai_deinit()
     // }
 }
 
-void ble_init()
+static void spiffs_init()
 {
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false};
 
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    switch (ret)
+    {
+    case ESP_OK:
+        break;
+    case ESP_FAIL:
+        ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        halt();
+        break;
+    case ESP_ERR_NOT_FOUND:
+        ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        halt();
+        break;
+    default:
+        ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        halt();
+    }
+    size_t total = 0, used = 0;
+    ESP_ERROR_CHECK(esp_spiffs_info(NULL, &total, &used));
+    ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+}
+static void spiffs_deinit()
+{
 }
 
-void ble_deinit()
-{
-    
-}
+
 
 static void report(lua_State *L, int status)
 {
-    if (status == LUA_OK) return;
+    if (status == LUA_OK)
+        return;
 
     const char *msg = lua_tostring(L, -1);
     printf("%s\n", msg);
@@ -212,38 +232,6 @@ static void halt()
         vTaskDelay(1000);
 }
 
-static void mount_spiffs()
-{
-    esp_vfs_spiffs_conf_t conf = {
-      .base_path = "/lua",
-      .partition_label = NULL,
-      .max_files = 5,
-      .format_if_mount_failed = false
-    };
-
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-    switch (ret)
-    {
-        case ESP_OK:
-            break;
-        case ESP_FAIL:
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-            halt();
-            break;
-        case ESP_ERR_NOT_FOUND:
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-            halt();
-            break;
-        default:
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
-            halt();
-    }
-    size_t total = 0, used = 0;
-    ESP_ERROR_CHECK(esp_spiffs_info(NULL, &total, &used));
-    ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-}
-
-
 void mainTask(void *arg)
 {
     lua_State *L = luaL_newstate();
@@ -253,7 +241,7 @@ void mainTask(void *arg)
 
     int r = luaL_loadfilex(L, "/sdcard/main.lua", NULL);
     if (r != LUA_OK)
-        printf("Failed to execute /lua/main.lua\n");
+        printf("Failed to execute main.lua\n");
     else
         r = lua_pcall(L, 0, LUA_MULTRET, 0);
 
@@ -268,12 +256,16 @@ void mainTask(void *arg)
         fflush(stdout);
         vTaskDelay(100);
     }
+
+    // Deinit peripherals in reverse of initialization
+    ble_deinit();
+    spiffs_deinit();
+    twai_deinit();
     motor_deinit();
     radio_deinit();
     sdcard_deinit();
     spi_deinit();
-    twai_deinit();
-    ble_deinit();
+    halt();
 }
 
 void app_main()
@@ -281,19 +273,18 @@ void app_main()
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
     printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
     spi_init();
     sdcard_init();
     radio_init();
     motor_init();
     twai_init();
+    spiffs_init();
     ble_init();
-
-    mount_spiffs();
 
     xTaskCreate(mainTask, "mainTask", 0x10000, NULL, 5, NULL);
 }
