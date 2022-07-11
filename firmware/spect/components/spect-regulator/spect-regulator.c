32732bcb4ee57efed29cad75ff8943ed1c14d249
Connor Rigby <connor@Connors-MBP.hsd1.co.comcast.net>
2022-07-10 19:15:08 -0600

    more firmware shit

---
+++ firmware/spect/components/spect-regulator/spect-regulator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "spect-regulator.h"
+
+esp_err_t spect_regulator_init(spect_regulator_cfg_t* cfg, spect_regulator_t** out_ctx)
+{
+  return ESP_OK;
+}
+
+esp_err_t spect_regulator_enable(spect_regulator_t* ctx)
+{
+  return ESP_OK;
+}
+
+esp_err_t spect_regulator_disable(spect_regulator_t* ctx)
+{
+  return ESP_OK;
+}
